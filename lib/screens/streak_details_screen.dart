38378dc1f8b30796636679ae9d312995680adb1e
akramul47 <akramul47@gmail.com>
2026-01-08 14:52:35 +0600

    feat: add hover animations for streak cards

--- lib/screens/streak_details_screen.dart
+++ lib/screens/streak_details_screen.dart
@@ -218,8 +218,8 @@ class _StreakDetailsScreenState extends State<StreakDetailsScreen>
           'Day Streak',
           style: GoogleFonts.outfit(
             fontSize: 16,
-            fontWeight: FontWeight.w500,
-            color: isDark ? _mutedOrange : AppTheme.textMedium,
+            fontWeight: FontWeight.w600,
+            color: isDark ? _fireOrange : AppTheme.textDark,
             letterSpacing: 1.0,
           ),
         ),
@@ -250,7 +250,7 @@ class _StreakDetailsScreenState extends State<StreakDetailsScreen>
       style: GoogleFonts.outfit(
         fontSize: 20,
         fontWeight: FontWeight.w600,
-        color: isDark ? _fireOrange.withOpacity(0.9) : AppTheme.textDark,
+        color: isDark ? _fireOrange : AppTheme.textDark,
         height: 1.4,
       ),
     );
@@ -435,42 +435,12 @@ class _StreakDetailsScreenState extends State<StreakDetailsScreen>
     required Color iconColor,
     required bool isDark,
   }) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-      decoration: BoxDecoration(
-        color: isDark
-            ? Colors.white.withOpacity(0.05)
-            : Colors.white.withOpacity(0.7),
-        borderRadius: BorderRadius.circular(12),
-        border: Border.all(
-          color: isDark
-              ? Colors.white.withOpacity(0.1)
-              : AppTheme.primaryColor.withOpacity(0.1),
-        ),
-      ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Icon(icon, color: iconColor, size: 24),
-          const SizedBox(height: 6),
-          Text(
-            value,
-            style: GoogleFonts.outfit(
-              fontSize: 20,
-              fontWeight: FontWeight.w700,
-              color: isDark ? Colors.white : AppTheme.textDark,
-            ),
-          ),
-          Text(
-            label,
-            style: GoogleFonts.outfit(
-              fontSize: 11,
-              fontWeight: FontWeight.w500,
-              color: isDark ? Colors.white60 : AppTheme.textMedium,
-            ),
-          ),
-        ],
-      ),
+    return _HoverStatCard(
+      icon: icon,
+      value: value,
+      label: label,
+      iconColor: iconColor,
+      isDark: isDark,
     );
   }
 
@@ -775,3 +745,135 @@ class _MandalaPatternPainter extends CustomPainter {
 
 double cos(double radians) => math.cos(radians);
 double sin(double radians) => math.sin(radians);
+
+/// Interactive stat card with hover animation
+class _HoverStatCard extends StatefulWidget {
+  final IconData icon;
+  final String value;
+  final String label;
+  final Color iconColor;
+  final bool isDark;
+
+  const _HoverStatCard({
+    required this.icon,
+    required this.value,
+    required this.label,
+    required this.iconColor,
+    required this.isDark,
+  });
+
+  @override
+  State<_HoverStatCard> createState() => _HoverStatCardState();
+}
+
+class _HoverStatCardState extends State<_HoverStatCard>
+    with SingleTickerProviderStateMixin {
+  bool _isHovering = false;
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(
+      begin: 1.0,
+      end: 1.08,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      onEnter: (_) {
+        setState(() => _isHovering = true);
+        _controller.forward();
+      },
+      onExit: (_) {
+        setState(() => _isHovering = false);
+        _controller.reverse();
+      },
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return Transform.scale(
+            scale: _scaleAnimation.value,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+              decoration: BoxDecoration(
+                color: _isHovering
+                    ? (widget.isDark
+                          ? Colors.white.withAlpha(25)
+                          : Colors.white.withAlpha(230))
+                    : (widget.isDark
+                          ? Colors.white.withAlpha(13)
+                          : Colors.white.withAlpha(179)),
+                borderRadius: BorderRadius.circular(12),
+                border: Border.all(
+                  color: _isHovering
+                      ? widget.iconColor.withAlpha(100)
+                      : (widget.isDark
+                            ? Colors.white.withAlpha(26)
+                            : AppTheme.primaryColor.withAlpha(26)),
+                  width: _isHovering ? 1.5 : 1.0,
+                ),
+                boxShadow: _isHovering
+                    ? [
+                        BoxShadow(
+                          color: widget.iconColor.withAlpha(60),
+                          blurRadius: 12,
+                          spreadRadius: 2,
+                        ),
+                      ]
+                    : [],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  AnimatedContainer(
+                    duration: const Duration(milliseconds: 200),
+                    child: Icon(
+                      widget.icon,
+                      color: widget.iconColor,
+                      size: _isHovering ? 28 : 24,
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  Text(
+                    widget.value,
+                    style: GoogleFonts.outfit(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: widget.isDark ? Colors.white : AppTheme.textDark,
+                    ),
+                  ),
+                  Text(
+                    widget.label,
+                    style: GoogleFonts.outfit(
+                      fontSize: 11,
+                      fontWeight: FontWeight.w500,
+                      color: widget.isDark
+                          ? Colors.white60
+                          : AppTheme.textMedium,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
